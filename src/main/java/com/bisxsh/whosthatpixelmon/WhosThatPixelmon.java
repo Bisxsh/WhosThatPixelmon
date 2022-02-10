7eab8cc34ee03b902b3fe0287e69259768d92dd8
Bisesh <bisesh.sitaula@gmail.com>
2022-02-10 20:38:19 +0000

    Updated gitignore and version number

--- src/main/java/com/bisxsh/whosthatpixelmon/WhosThatPixelmon.java
+++ src/main/java/com/bisxsh/whosthatpixelmon/WhosThatPixelmon.java
@@ -48,7 +48,7 @@ public class WhosThatPixelmon {
 
     public static final String MOD_ID = "whosthatpixelmon";
     public static final String MOD_NAME = "WhosThatPixelmon";
-    public static final String VERSION = "1.0-SNAPSHOT";
+    public static final String VERSION = "1.2.0";
     private static WhosThatPixelmon INSTANCE = null;
 
     @Inject
